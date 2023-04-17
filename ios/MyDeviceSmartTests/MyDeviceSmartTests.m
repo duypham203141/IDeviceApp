021a4e49c27b0c15f3f43b89028cfb99e03ae5a7
henry.pham <henry.pham@websynergies.com>
2023-04-17 23:05:37 +0700

    rename

--- ios/MyDeviceSmartTests/MyDeviceSmartTests.m
+++ ios/MyDeviceSmartTests/MyDeviceSmartTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AwesomeProject2Tests : XCTestCase
+@interface MyDeviceSmartTests : XCTestCase
 
 @end
 
-@implementation AwesomeProject2Tests
+@implementation MyDeviceSmartTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
